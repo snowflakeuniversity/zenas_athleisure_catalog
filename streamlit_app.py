e7fc03beedd6c60e44af077efee5f09f8e8f3373
Florence D. Neige <101849240+snowflakeuniversity@users.noreply.github.com>
2022-06-16 19:40:50 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import snowflake.connector
+import pandas
 
 streamlit.title('Zena\'s Athleisure Catalog')
 
@@ -7,11 +8,14 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
 # put your sql query here
-my_cur.execute("select top 1 direct_url from catalog;")
-my_data_row = my_cur.fetchone()
+my_cur.execute("select top 5 * from catalog;")
+my_catalog = my_cur.fetchall()
+#my_data_row = my_cur.fetchone()
 
-streamlit.text(my_data_row)
-streamlit.dataframe(my_data_row)
+
+#streamlit.text(my_data_row)
+streamlit.dataframe(my_catalog)
+
 
 streamlit.image(
             'https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/orange_sweatsuit.png',
@@ -19,11 +23,7 @@ streamlit.image(
             caption="Buy this wonderful product"
         )
 
-streamlit.image(
-            my_data_row,
-            width=400,
-            caption="Buy this wonderful product"
-        )
+
 
 
 
