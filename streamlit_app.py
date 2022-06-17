5fae8bdb0819441f840394619af23ba71bf89bad
Florence D. Neige <101849240+snowflakeuniversity@users.noreply.github.com>
2022-06-16 19:06:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,11 @@ streamlit.title('Zena\'s Athleisure Catalog')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+
+# put your sql query here
+my_cur.execute("select top 1 direct_url from catalog;")
 my_data_row = my_cur.fetchone()
+
+
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
