91b1cedc65a96c81cea3121cce84ae19963fbf8c
Florence D. Neige <101849240+snowflakeuniversity@users.noreply.github.com>
2022-06-16 19:12:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import snowflake.connector
 from PIL import Image
 
 
-
-
 streamlit.title('Zena\'s Athleisure Catalog')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -13,8 +11,13 @@ my_cur = my_cnx.cursor()
 # put your sql query here
 my_cur.execute("select top 1 direct_url from catalog;")
 my_data_row = my_cur.fetchone()
-image = Image.open(my_data_row)
-st.image(image, caption='Get this great new item!')
+
+st.image(
+            my_data_row,
+            width=400,
+            caption="uy this wonderful product"
+        )
+
 
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
