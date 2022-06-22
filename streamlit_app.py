69f1fd85fd83e3ed2a5d606f018f5b2f185e7f9b
Florence D. Neige <101849240+snowflakeuniversity@users.noreply.github.com>
2022-06-21 17:00:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,24 +16,23 @@ my_catalog = my_cur.fetchall()
 df = pandas.DataFrame(my_catalog)
 
 # temp write the dataframe to the page so I Can see what I am working with
-#streamlit.write(df)
+# streamlit.write(df)
 
 # put the first column into a list
 color_list = df[0].values.tolist()
 # print(color_list)
 
 # Let's put a pick list here so they can pick the color 
-option = 'orange'
 option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
 
-#streamlit.write('You selected:', option)
-
+# We'll build the image caption now, since we can 
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!' 
 
-# use the option
+# use the option selected to go back and get all the info from the database
 my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
 image_url = my_cur.fetchone()[0]
 df2 = my_cur.fetchone()
+streamlit.write("is the next line none?")
 streamlit.write(df2)
 
 
